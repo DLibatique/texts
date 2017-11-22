8382c9e988e7c64b8cca0ed013b8e4f807e72513
Daniel Libatique <daniel.libatique@gmail.com>
2017-11-22 18:05:05 -0500

    updated code

--- iliad/parser.py
+++ iliad/parser.py
@@ -1,8 +1,12 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import xml.etree.ElementTree as ET
 tree = ET.parse('iliad_1_1-32.xml')
 root = tree.getroot()
 
-file = open('iliad_1_1-32.txt', 'w')
+file = open('iliad_1_1-32.txt','w')
 for l in root.iter('l'):
-	file.write(str(l.text))
+	file.write(str(l.text) + "\n")
 file.close()
