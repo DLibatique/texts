f4d43b58839fe7e339b90900df164461d74a65f6
Daniel Libatique <daniel.libatique@gmail.com>
2017-12-20 22:51:50 -0500

    added exception

--- xml/cic_test.py
+++ xml/cic_test.py
@@ -3,5 +3,12 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('cic.fam_lat.xml')
 root = tree.getroot()
 
+while True:
+	try:
+		for name in root.iter('name'):
+			print(root.tag, name.text)
+	except xml.etree.ElementTree.ParseError:
+		pass
+
 for name in root.iter('name'):
-	print(root.tag, name.text)
+	print(name.text)
