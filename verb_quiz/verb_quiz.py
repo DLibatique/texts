96072e89940b09b02a325b328ddda132ea2fd5e5
Daniel Libatique <daniel.libatique@gmail.com>
2017-12-30 22:25:26 -0500

    changed dict to list, filled out code

--- verb_quiz/verb_quiz.py
+++ verb_quiz/verb_quiz.py
@@ -1,21 +1,34 @@
 import random
 
-verbs = {
-    'ἄγω':['ἄξω', 'ἤγαγον', 'ἦχα', 'ἦγμαι','ἤχθην'],
-    'βαίνω':['βήσομαι', 'ἔβην', 'βέβηκα', None, None],
-    'βάλλω':['βαλῶ', 'ἔβαλον', 'βέβληκα', 'βέβλημαι', 'ἐβλήθην'],
-    'δίδωμι':['δώσω', 'ἔδωκα', 'δέδωκα', 'δέδομαι', 'ἐδόθην'],
-    'ἔρχομαι':['ἐλεύσομαι', 'ἦλθον', 'ἐλήλυθα', None, None],
-    'ἔχω':['ἕξω', 'ἔσχον', 'ἔσχηκα', None, None],
-    'ἵστημι':['στήσω', 'ἔστην/ἔστησα', 'ἕστηκα', 'ἕσταμαι', 'ἐστάθην']
-}
+def quiz():
 
-#group the verbs' 2nd through 6th principal parts
-principal_parts = list(verbs.values())
+    verbs = [
+        ['ἄγω', 'ἄξω', 'ἤγαγον', 'ἦχα', 'ἦγμαι','ἤχθην'],
+        ['βαίνω', 'βήσομαι', 'ἔβην', 'βέβηκα', None, None],
+        ['βάλλω', 'βαλῶ', 'ἔβαλον', 'βέβληκα', 'βέβλημαι', 'ἐβλήθην'],
+        ['δίδωμι', 'δώσω', 'ἔδωκα', 'δέδωκα', 'δέδομαι', 'ἐδόθην'],
+        ['ἔρχομαι', 'ἐλεύσομαι', 'ἦλθον', 'ἐλήλυθα', None, None],
+        ['ἔχω', 'ἕξω', 'ἔσχον', 'ἔσχηκα', None, None],
+        ['ἵστημι', 'στήσω', 'ἔστην/ἔστησα', 'ἕστηκα', 'ἕσταμαι', 'ἐστάθην']
+	]
 
-#choose random principal part from random verb
-random_principal_part = random.choice(principal_parts[random.randint(0,4)])
+    #choose a verb
+    selected_verb = random.choice(verbs)
+	
+    #choose a principal part of said verb, 2nd through 6th
+    random_pp = selected_verb[random.randint(1,5)]
 
-#prompt to quiz taker
-print('What is the first principal part of', random_principal_part, '?')
+    #ensure that random_pp is assigned to an actual verbal form
+    while random_pp == None:
+        random_pp = selected_verb[random.randint(1,5)]
 
+    #ask user to answer
+    answer = input('What is the first principal part of ' + random_pp + '? --> ')
+
+    #if user is incorrect, prompt again
+    if answer == selected_verb[0]:
+        print('Correct!')
+    else:
+        answer = input('Try again! --> ')
+
+quiz()
